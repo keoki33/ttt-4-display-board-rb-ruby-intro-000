5c24d6cd56e10d3a25c07bb0d398d6768ed53559
George Wong <gwong323@yahoo.com>
2019-01-24 19:00:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-board = [" "," "," "," ","x"," "," "," "," "]
+board = ["x"," "," "," "," "," "," "," "," "]
 display_board(board)
