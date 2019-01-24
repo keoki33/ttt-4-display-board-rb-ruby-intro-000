16733f9921e455263f86575e6cf7b770f886e1fe
George Wong <gwong323@yahoo.com>
2019-01-24 18:52:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
 board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+
+display_board
